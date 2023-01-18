076658bb01d052176c9b9ce1ece346e77b5f3e44
Samin Shahriar Tokey <saminshahriar@rocketmail.com>
2023-01-18 14:11:31 -0500

    Initial Git Repo with Git-Ignore for UE5

---
+++ Source/Prey/PreyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PreyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PREY_API APreyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
